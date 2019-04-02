04a2eb3c5136622826e7e7ffb0d0088c1f404a7b
fedejimenez <fedej_mdp@hotmail.com>
2019-04-02 14:14:15 -0300

    add styles and product details

--- models/product.js
+++ models/product.js
@@ -26,6 +26,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -37,4 +38,11 @@ module.exports = class Product {
     static fetchAll(callback) { // call method from the classs, not from an object
         getProductsFromFile(callback);
     }
-}
+
+    static findById(id, callback) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            callback(product);
+        });
+    }
+};
