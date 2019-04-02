6da9b7cb37190941b87c5f618b22b31bc13afa86
fedejimenez <fedej_mdp@hotmail.com>
2019-04-02 15:07:42 -0300

    add cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -36,6 +37,14 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
         path: '/cart'
     });
+}
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.redirect('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get('/products/:id', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
