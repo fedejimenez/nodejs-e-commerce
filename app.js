c201d40c77d1af527b6443abc37b109ca73fd8ca
fedejimenez <fedej_mdp@hotmail.com>
2019-04-07 17:27:30 -0300

    add userId in the product

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+    User.findById('5caa5a091c9d4400003d33a8')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -33,5 +33,5 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
-  app.listen(3000);
+    app.listen(3000);
 });
--- models/user.js
+++ models/user.js
@@ -1,16 +1,44 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User {
+    constructor(username, email) {
+        this.name = name;
+        this.email = name;
+        this._id = id ? new mongodb.ObjectId(id) : null;
+    }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+    save() {
+        const db = getDb();
+        let dbOperation;
+        if (this._id) {
+            dbOperation = db
+                .collection('users')
+                .updateOne({ _id: this._id }, { $set: this });
+        } else {
+            dbOperation = db.collection('users').insertOne(this);
+        }
+        return dbOperation
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
+    static findById(userId) {
+        const db = getDb();
+        return db
+            .collection('users')
+            .findOne({ _id: new mongodb.ObjectId(userId) })
+            .then(user => {
+                console.log(user);
+                return user;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+}
 module.exports = User;
