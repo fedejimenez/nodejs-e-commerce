561bbe5b10a3051cabf4dd490153ec1c857bf12e
fedejimenez <fedej_mdp@hotmail.com>
2019-04-14 11:24:04 -0300

    refactor displaying orders and clear cart with mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -97,14 +97,17 @@ exports.postOrder = (req, res, next) => {
             return order.save();
         })
         .then(result => {
+            return req.user.clearCart();
+        })
+        .then(() => {
             res.redirect('/orders');
         })
         .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user
-        .getOrders()
+    Order
+        .find({ "user.userId": req.user._id })
         .then(orders => {
             res.render('shop/orders', {
                 path: '/orders',
