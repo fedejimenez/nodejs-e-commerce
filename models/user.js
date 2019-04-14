e468cfe18fa23edafe89e130de17cf7f9d6c9caf
fedejimenez <fedej_mdp@hotmail.com>
2019-04-13 22:19:21 -0300

    refactor product model for using mongoose

--- models/user.js
+++ models/user.js
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+});
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
