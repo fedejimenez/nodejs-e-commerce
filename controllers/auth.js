ce4bf32cbd56749b15b737159ba4e5f49b96053c
fedejimenez <fedej_mdp@hotmail.com>
2019-04-14 13:40:53 -0300

    install express-session and config files

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: req.isLoggedIn
     });
+};
+
+exports.postLogin = (req, res, next) => {
+    req.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    res.redirect('/');
 };
