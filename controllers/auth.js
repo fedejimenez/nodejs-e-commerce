5de82f7807783559fe4eac7cbc07409f747c949d
fedejimenez <fedej_mdp@hotmail.com>
2019-04-27 13:09:34 -0300

    Add signup view, route, and controller method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,17 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    // const isLoggedIn = req
-    // .get('Cookie')
-    // .split(';'[0])
-    // .trim()
-    // .split('='[1] === 'true');
-
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
+        isAuthenticated: false
+    });
+};
+
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
         isAuthenticated: false
     });
 };
@@ -25,6 +27,10 @@ exports.postLogin = (req, res, next) => {
             });
         })
         .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+
 };
 
 exports.postLogout = (req, res, next) => {
