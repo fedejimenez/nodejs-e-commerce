c064d773cdd234433b5bdf43f6087496fea9350d
fedejimenez <fedej_mdp@hotmail.com>
2019-04-27 23:33:25 -0300

    configure sendgrid for sending email after sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
-const bcrypt = require('bcryptjs')
+require('dotenv').config(); // use env variables
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
 const User = require('../models/user');
+
+// for nodemailer
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: process.env.SENDGRID_API_KEY
+    }
+}));
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -84,7 +95,16 @@ exports.postSignup = (req, res, next) => {
                     return user.save();
                 })
                 .then(result => {
-                    res.redirect('/');
+                    res.redirect('/login');
+                    return transporter.sendMail({
+                        to: email,
+                        from: 'node@e-commerce.com',
+                        subject: 'Signup succeeded!',
+                        html: '<h1>You successfully signed up!</h1>'
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
                 });
         })
         .catch(err => {
