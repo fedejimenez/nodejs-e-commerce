8478fbdabb85c5363e625f8fdcb18a140b32d1bc
fedejimenez <fedej_mdp@hotmail.com>
2019-05-19 16:50:36 -0300

    add and config pdfkit for creating pdf on the fly

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -147,7 +149,7 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.getInvoice = (req, res, next) => {
-    const orderId = req.params.oderId;
+    const orderId = req.params.orderId;
     Order.findById(orderId)
         .then(order => {
             if (!order) {
@@ -156,8 +158,22 @@ exports.getInvoice = (req, res, next) => {
             if (order.user.userId.toString() !== req.user._id.toString()) {
                 return next(new Error('Unauthorized'));
             }
-            const invoiceName = 'invoice-' + orderId + 'pdf';
+            const invoiceName = 'invoice-' + orderId + '.pdf';
             const invoicePath = path.join('data', 'invoices', invoiceName);
+
+            const pdfDoc = new PDFDocument();
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader(
+                'Content-Disposition',
+                'inline; filename="' + invoiceName + '"' // attachment instead of inline for download it 
+            );
+            pdfDoc.pipe(fs.createWriteStream(invoicePath)); // file generated on the fly
+            pdfDoc.pipe(res);
+
+            pdfDoc.text('Hello Wordl!');
+
+            pdfDoc.end();
+
             // fs.readFile(invoicePath, (err, data) => {
             //     if (err) {
             //         return next(err);
@@ -169,13 +185,9 @@ exports.getInvoice = (req, res, next) => {
             //     );
             //     res.send(data);
             // });
-            const file = fs.createReadStream(invoicePath);
-            res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader(
-                'Content-Disposition',
-                'inline; filename="' + invoiceName + '"'
-            );
-            file.pipe(res);
+
+            // const file = fs.createReadStream(invoicePath);
+            // file.pipe(res);
         })
         .catch(err => next(err));
 };
