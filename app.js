d4395650aab775db7af450e3837263779c650df4
fedejimenez <fedej_mdp@hotmail.com>
2019-05-19 14:44:13 -0300

    add filter for image mimetype

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ const fileStorage = multer.diskStorage({
     }
 });
 
+const fileFilter = (req, file, cb) => {
+    if (file.mymetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    cb(null, false);
+};
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -40,7 +47,7 @@ const authRoutes = require('./routes/auth');
 
 // Midlewares
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'my secret',
--- controllers/admin.js
+++ controllers/admin.js
@@ -21,7 +21,6 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.file;
     const price = req.body.price;
     const description = req.body.description;
-    console.log(imageUrl);
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
