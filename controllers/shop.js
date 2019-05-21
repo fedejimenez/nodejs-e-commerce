f81ee681c161fa64390349d76ae1c44006e713c7
fedejimenez <fedej_mdp@hotmail.com>
2019-05-20 21:46:59 -0300

    add same logic for products list

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,13 +9,30 @@ const Order = require('../models/order');
 const ITEMS_PER_PAGE = 4;
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    const page = +req.query.page || 1; // + converts to number
+    let totalItems;
+
+    Product
+        .find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
-            console.log(products);
             res.render('shop/product-list', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
+                pageTitle: 'Products',
+                path: '/products',
+                totalProducts: totalItems,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
