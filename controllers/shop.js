fe432ba26cefe498f9482c616ad267312aef6dea
fedejimenez <fedej_mdp@hotmail.com>
2019-05-20 21:39:37 -0300

    change to 4 items per page

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,7 +6,7 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 4;
 
 exports.getProducts = (req, res, next) => {
     Product.find()
