15c896b8f515c2c22549bf9eb5d453727ed18d3b
Federico Jimenez <fedeejimenez@gmail.com>
2020-04-15 19:28:18 +0100

    Add styling to login / signup and guest user

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,37 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
+const backdrop = document.querySelector(".backdrop");
+const sideDrawer = document.querySelector(".mobile-nav");
+const menuToggle = document.querySelector("#side-menu-toggle");
 
 function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
+  backdrop.style.display = "none";
+  sideDrawer.classList.remove("open");
 }
 
 function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
+  backdrop.style.display = "block";
+  sideDrawer.classList.add("open");
 }
 
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
+backdrop.addEventListener("click", backdropClickHandler);
+menuToggle.addEventListener("click", menuToggleClickHandler);
+
+// // LOGIN
+// var working = false;
+// $(".login").on("submit", function(e) {
+//   e.preventDefault();
+//   if (working) return;
+//   working = true;
+//   var $this = $(this),
+//     $state = $this.find("button > .state");
+//   $this.addClass("loading");
+//   $state.html("Authenticating");
+//   setTimeout(function() {
+//     $this.addClass("ok");
+//     $state.html("Welcome back!");
+//     setTimeout(function() {
+//       $state.html("Log in");
+//       $this.removeClass("ok loading");
+//       working = false;
+//     }, 4000);
+//   }, 3000);
+// });
